6bddbd612ef4198865cc6e197de70b93f58f9b60
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-02-26 22:14:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,11 +38,37 @@ const apartment = {
 
 /* --------------------------------- TASK 2 --------------------------------- */
 
-/*  */
+/* Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
+
+name - имя владельца, значение "Henry";
+phone - телефон, значение "982-126-1588";
+email - почта, значение "henry.carter@aptmail.com".
+Объявлена переменная apartment
+Значение переменной apartment это объект
+У объекта переменной apartment присутствуют свойства imgUrl, descr, rating, price и tags со значениями
+В объекте apartment есть свойство owner
+Значение свойства owner это объект
+В объекте owner есть свойство name
+Значение свойства name это "Henry"
+В объекте owner есть свойство phone
+Значение свойства phone это "982-126-1588"
+В объекте owner есть свойствао email
+Значение свойства email это "henry.carter@aptmail.com" */
 
 
 /* 
 
-
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  }
+};
 
 */
