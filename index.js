22fd98253a811899b732f17092f668809406391c
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-14 23:39:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -177,3 +177,44 @@ const lastTag = apartment.tags[2];
 
 
 
+/*------------------------- TASK 5 -------------------------*/
+
+/* 
+
+Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment используя синтаксис «квадратных скобок».
+
+aptRating - рейтинг;
+aptDescr - описание;
+aptPrice - цена;
+aptTags - теги.
+Объявлена переменная apartment
+Значение переменной apartment это объект
+Объявлена переменная aptRating
+Значение переменной aptRating это 4
+Объявлена переменная aptDescr
+Значение переменной aptDescr это "Spacious apartment in the city center"
+Объявлена переменная aptPrice
+Значение переменной aptPrice это 2153
+Объявлена переменная aptTags
+Значение переменной aptTags это ["premium", "promoted", "top"]
+
+*/
+
+/* 
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
+
+// Change code below this line
+const aptRating = apartment["rating"];
+const aptDescr = apartment["descr"];
+const aptPrice = apartment["price"];
+const aptTags = apartment["tags"];
+// Change code above this line
+
+*/
