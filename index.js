b7f9568803192547ee4d911b0acbc77c5728ec03
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-15 23:30:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -218,3 +218,201 @@ const aptTags = apartment["tags"];
 // Change code above this line
 
 */
+
+
+
+
+/*------------------------- TASK 6 -------------------------*/
+
+/* 
+
+Дополни код обновив значения свойств объекта apartment:
+
+цену в свойстве price на 5000;
+рейтинг квартиры в свойстве rating на 4.7;
+имя владельца во вложенном свойстве name на "Henry Sibola";
+массив тегов в свойстве tags добавив в конец строку "trusted".
+Объявлена переменная apartment
+Значение переменной apartment это объект
+Значение вложенного свойства price это число 5000
+Значение вложенного свойства rating это число 4.7
+Значение вложенного свойства name это строка "Henry Sibola"
+Значение вложенного свойства tags это массив ["premium", "promoted", "top", "trusted"]
+
+*/
+
+/* 
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = "Henry Sibola";
+apartment.tags.push("trusted"); 
+
+*/
+
+
+
+
+/*------------------------- TASK 7 -------------------------*/
+
+/* 
+
+Добавь объекту apartment несколько новых свойств:
+
+area - площадь в квадратных метрах, число 60;
+rooms - количество комнат, число 3;
+location - местоположение квартиры, обьект со следующими вложенными свойствами;
+country - страна, строка "Jamaica";
+city - город, строка "Kingston".
+Объявлена переменная apartment
+Значение переменной apartment это объект
+Значение вложенного свойства area это число 60
+Значение вложенного свойства rooms это число 3
+Значение вложенного свойства location это объект
+Значение вложенного свойства location.country это строка "Jamaica"
+Значение вложенного свойства location.city это строка "Kingston"
+
+*/
+
+/* 
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+apartment.area = 60;
+apartment.rooms = 3;
+
+apartment.location = {
+  country: "Jamaica",
+  city: "Kingston",
+}
+
+*/
+
+
+
+/*------------------------- TASK 8 -------------------------*/
+
+/* 
+
+Дополни код объявления объекта так, чтобы у него были свойства name, price, image и tags со значениями из переменных с аналогичными именами. Обязательно используй синтаксис коротких свойств.
+
+Объявлена переменная product
+Значение переменной product это объект
+Значение вложенного свойства name это строка "Repair Droid"
+Значение вложенного свойства price это число 2500
+Значение вложенного свойства image это строка "https://via.placeholder.com/640x480"
+Значение вложенного свойства tags это массив ["on sale", "trending", "best buy"]
+
+*/
+
+/* 
+
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  name,
+  price,
+  image,
+  tags,
+};
+
+*/
+
+
+
+
+/*------------------------- TASK 9 -------------------------*/
+
+/* 
+
+Дополни код объявления объекта credentials так, чтобы в результате у него были два свойства: email и password, имена которых хранятся в переменных emailInputName и passwordInputName.
+
+Значением свойства email должна быть строка "henry.carter@aptmail.com", а значением свойства password - строка "jqueryismyjam".
+
+Объявлена переменная credentials
+Значение переменной credentials это объект
+В объекте credentials есть свойство email
+Значение вложенного свойства email это строка "henry.carter@aptmail.com"
+В объекте credentials есть свойство password
+Значение вложенного свойства password это строка "jqueryismyjam"
+
+*/
+
+/* 
+
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+[emailInputName]:"henry.carter@aptmail.com",
+[passwordInputName]:"jqueryismyjam",
+};
+
+*/
+
+
+
+/*------------------------- TASK 10 -------------------------*/
+
+/* 
+
+Перебери объект apartment используя цикл for...in и запиши в массив keys все его ключи, а в массив values все значения его свойств.
+
+Объявлена переменная apartment
+Значение переменной apartment это объект
+Объявлена переменная keys
+Значение переменной keys это массив ["descr", "rating", "price"]
+Объявлена переменная values
+Значение переменной values это массив ["Spacious apartment in the city center", 4, 2153]
+
+*/
+
+/* 
+
+const apartment = {
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+  };
+  
+  const keys = [];
+  const values = [];
+  
+  // Change code below this line
+  
+  for (let key in apartment) {
+    keys.push(key) // Добавление в массив keys ключа из обекта apartment
+    values.push(apartment[key]) // Добавление в массив values значения по ключу из обекта apartment
+  };
+    console.log(values); // вывод массива values
+    console.log(keys)
+
+*/
