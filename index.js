1477b104e07159dacc6f9ff78adc0b6003acac65
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-16 23:53:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -416,3 +416,82 @@ const apartment = {
     console.log(keys)
 
 */
+
+
+
+/*------------------------- TASK 11 -------------------------*/
+
+/* 
+
+Выполни рефакторинг решения предыдущего задания добавив в цикл for...in проверку на собственное свойство.
+
+Объявлена переменная advert.
+Значение переменной advert это объект.
+Объявлена переменная apartment.
+Значение переменной apartment это объект.
+Объявлена переменная keys.
+Значение переменной keys это массив ["descr", "rating", "price"].
+Объявлена переменная values.
+Значение переменной values это массив ["Spacious apartment in the city center", 4, 2153].
+
+
+*/
+
+/* 
+
+const keys = [];
+const values = [];
+const advert = {
+  service: "apt",
+};
+const apartment = Object.create(advert);
+apartment.descr = "Spacious apartment in the city center";
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+  
+if (apartment.hasOwnProperty(key)) {
+    keys.push(key);
+  values.push(apartment[key]);
+}
+};
+
+*/
+
+
+
+/*------------------------- TASK 12 -------------------------*/
+
+/* 
+
+Напиши функцию countProps(object), которая считает и возвращает количество собственных свойств объекта в параметре object. Используй переменную propCount для хранения количества свойств объекта.
+
+Объявлена функция countProps(object)
+Вызов countProps({}) возвращает 0
+Вызов countProps({ name: "Mango", age: 2 }) возвращает 2
+Вызов countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }) возвращает 3
+Функция подсчитывает только собственные свойства объекта
+
+*/
+
+/* 
+
+function countProps(object) {
+  let propCount = 0;
+
+  for (let prop in object) {
+    if (object.hasOwnProperty(prop)) {
+      propCount += 1;
+    }
+  }
+
+  return propCount;
+}
+
+console.log(countProps({}));
+console.log(countProps({name: "Mango", age: 2}));
+console.log(countProps({mail: "poly@mail.com", isOnline: true, score: 500}));
+
+*/
+
