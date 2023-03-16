af7186ca8ec48c195271aeac9a47ba9ff7bada1c
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-16 23:53:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -495,3 +495,60 @@ console.log(countProps({mail: "poly@mail.com", isOnline: true, score: 500}));
 
 */
 
+
+
+/*------------------------- TASK 13 -------------------------*/
+
+/* 
+
+Перебери объект apartment используя метод Object.keys() и цикл for...of. Запиши в переменную keys массив ключей собственных свойств объекта apartment, и добавь в массив values все значения его свойств.
+
+Объявлена переменная apartment.
+Значение переменной apartment это объект.
+Объявлена переменная keys.
+Значение переменной keys это массив ["descr", "rating", "price"].
+Значение переменной keys получено с помощью метода Object.keys().
+Объявлена переменная values.
+Значение переменной values это массив ["Spacious apartment in the city center", 4, 2153].
+Значение переменной values получено с помощью цикла for...of.
+
+*/
+
+/* 
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const keys = Object.keys(apartment);
+const values = [];
+
+for (let key of keys) {
+  values.push(apartment[key]);
+}
+
+
+console.log(keys);
+console.log(values);
+
+*/
+
+
+
+/*------------------------- TASK 14 -------------------------*/
+
+/* 
+
+Выполни рефакторинг функции countProps(object) используя метод Object.keys() и, возможно, но необязательно, цикл for...of.
+
+Объявлена функция countProps(object)
+Вызов countProps({}) возвращает 0
+Вызов countProps({ name: "Mango", age: 2 }) возвращает 2
+Вызов countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }) возвращает 3
+Функция подсчитывает только собственные свойства объекта
+Функция использует метод Object.keys() и, возможно, цикл for...of
+
+*/
+
