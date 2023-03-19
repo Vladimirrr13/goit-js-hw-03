f6df012e990bc0499819d49262825131ad67f2bb
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-19 23:55:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,49 @@ const apartment = {
 
 
 
+
+/*------------------------- TASK 18 -------------------------*/
+
+/* 
+
+Напиши функцию getProductPrice(productName) которая принимает один параметр productName - название продукта. Функция ищет объект продукта с таким именем (свойство name) в массиве products и возвращает его цену (свойство price). Если продукт с таким названием не найден, функция должна возвращать null.
+
+Объявлена функция getProductPrice(productName).
+Вызов getProductPrice("Radar") возвращает 1300.
+Вызов getProductPrice("Grip") возвращает 1200.
+Вызов getProductPrice("Scanner") возвращает 2700.
+Вызов getProductPrice("Droid") возвращает 400.
+Вызов getProductPrice("Engine") возвращает null.
+
+*/
+
+/* 
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.price;
+    }
+  }
+  return null;
+}
+
+console.log(getProductPrice("Radar"));
+console.log(getProductPrice("Grip"));
+console.log(getProductPrice("Scanner"));
+console.log(getProductPrice("Droid"));
+
+*/
+
+
+
 /* --------------------------------- TASK 2 --------------------------------- */
 
 /* Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
